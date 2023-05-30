41c3544a5d065b22e49df9d31a093476656d1ee7
Brent <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-05-30 11:59:05 -0700

    init repo

---
+++ models/index.js
@@ -0,0 +1,24 @@
+const User = require('./User');
+const Project = require('./Project');
+const Post = require('./Post');
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+User.hasMany(Project, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Project.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+
+
+module.exports = { User, Project, Post };
