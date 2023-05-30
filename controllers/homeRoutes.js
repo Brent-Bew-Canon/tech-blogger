c20507523a44eb947ea2e00477d830df174a3560
Brent <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-05-30 15:35:43 -0700

    modified partial for create post on dashboard

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -27,6 +27,27 @@ router.get('/', async (req, res) => {
   }
 });
 
+
+// Use withAuth middleware to prevent access to route
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('dashboard', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/project/:id', async (req, res) => {
   try {
     const projectData = await Project.findByPk(req.params.id, {
